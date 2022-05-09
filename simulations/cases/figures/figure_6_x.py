75b79426b4795178ab24a13f875df8cf1c336e6c
Lukas Forst <lukas@forst.dev>
2022-05-09 10:18:36 +0200

    best worst case

--- simulations/cases/figures/figure_6_x.py
+++ simulations/cases/figures/figure_6_x.py
@@ -1,5 +1,5 @@
 from fides.evaluation.ti_aggregation import AverageConfidenceTIAggregation
-from fides.evaluation.ti_evaluation import MaxConfidenceTIEvaluation
+from fides.evaluation.ti_evaluation import DistanceBasedTIEvaluation
 from simulations.environment import generate_and_run
 from simulations.model import SimulationConfiguration
 from simulations.peer import PeerBehavior
@@ -11,7 +11,7 @@ if __name__ == '__main__':
         malicious_targets=1,
         malicious_peers_lie_about_targets=1.0,
         peers_distribution={
-            PeerBehavior.CONFIDENT_CORRECT: 3,
+            PeerBehavior.CONFIDENT_CORRECT: 2,
             PeerBehavior.UNCERTAIN_PEER: 0,
             PeerBehavior.CONFIDENT_INCORRECT: 0,
             PeerBehavior.MALICIOUS_PEER: 6,
@@ -20,11 +20,11 @@ if __name__ == '__main__':
         malicious_peers_lie_since=25,
         service_history_size=100,
         pre_trusted_peers_count=2,
-        initial_reputation=0.95,
+        initial_reputation=0.0,
         local_slips_acts_as=PeerBehavior.UNCERTAIN_PEER,
-        evaluation_strategy=MaxConfidenceTIEvaluation(),
+        evaluation_strategy=DistanceBasedTIEvaluation(),
         ti_aggregation_strategy=AverageConfidenceTIAggregation()
     )
 
     result = generate_and_run(simulation_configuration)
-    plot_simulation_result(result)
+    plot_simulation_result(result, save_output='best_worst_case.png')
